c55caf856e716ac2cc30555273224088563e58d5
sudo-victor <victor.souto@hotmail.com>
2021-06-10 21:19:15 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DIO
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int count = 5;
+
+      for (int i = 0; i < count; i++)
+      {
+        Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
+      }
+    }
+  }
+}
